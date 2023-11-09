6297a33518ea7bbc4a7eb1128e210b5efefe2b57
RiyashameedM <97871624+RiyashameedM@users.noreply.github.com>
2023-11-09 18:30:22 +0530

    Add files via upload

---
+++ ListViewMaui/Model/GalleryInfo.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListViewMaui
+{
+    public class ListViewGalleryInfo : INotifyPropertyChanged
+    {
+        #region Fields
+
+        private ImageSource image;
+        private string imageTitle;
+
+        #endregion
+
+        #region Constructor
+
+        public ListViewGalleryInfo()
+        {
+
+        }
+
+        #endregion
+
+        #region Properties
+
+        public ImageSource Image
+        {
+            get { return image; }
+            set
+            {
+                image = value;
+                OnPropertyChanged("Image");
+            }
+        }
+
+        public string ImageTitle
+        {
+            get { return imageTitle; }
+            set
+            {
+                imageTitle = value;
+                OnPropertyChanged("ImageTitle");
+            }
+        }
+        #endregion
+
+        #region Interface Member
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string name)
+        {
+            if (this.PropertyChanged != null)
+                this.PropertyChanged(this, new PropertyChangedEventArgs(name));
+        }
+
+        #endregion
+    }
+}
---
+++ ListViewMaui/ViewModel/ViewModel.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListViewMaui
+{
+    public class ListViewGridLayoutViewModel : INotifyPropertyChanged
+    {
+        #region Fields
+
+        private ObservableCollection<ListViewGalleryInfo> galleryinfo;
+
+        #endregion
+
+        #region Constructor
+
+        public ListViewGridLayoutViewModel()
+        {
+            GenerateSource();
+        }
+
+        #endregion
+
+        #region Properties
+
+        public ObservableCollection<ListViewGalleryInfo> GalleryInfo
+        {
+            get { return galleryinfo; }
+            set { this.galleryinfo = value; }
+        }
+
+        #endregion
+
+        #region ItemSource
+
+        public void GenerateSource()
+        {
+            ListViewGalleryInfoRepository bookRepository = new ListViewGalleryInfoRepository();
+            galleryinfo = bookRepository.GetGalleryInfo();
+        }
+
+        #endregion
+
+        #region INotifyPropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string name)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+        }
+
+        #endregion
+    }
+}
